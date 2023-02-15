513ac1d26b841cb1e088e8094988a72f0699102a
ʀᴇᴅᴢᴇᴏꭗ <105351945+REDZEOX@users.noreply.github.com>
2023-02-15 10:51:29 +0530

    🕯️✨ʀᴇᴅᴢᴇᴏꭗ✨🕯️

--- Commands/Mods/charlist.js
+++ Commands/Mods/charlist.js
@@ -6,7 +6,7 @@ module.exports = {
   usage: "setchar 0/1/2/3/4/5/6/7",
   react: "🎀",
   start: async (Miku, m, { text, prefix ,modStatus}) => {
-    let txt = `       『  *Bot Character list*  』\n\n\n_0 - Atlas MD_\n\n_1 - Power MD_\n\n_2 - Makima MD_\n\n_3 - Denji MD_\n\n_4 - Zero Two_\n\n_5 - Chika MD_\n\n_6 - Miku MD_\n\n_7 - Marin Kitagawa_\n\n\nExample: *${prefix}setchar 0*\n`;
+    let txt = `       『  *Bot Character list*  』\n\n\n_0 - Atlas MD_\n\n_1 - Power MD_\n\n_2 - Makima MD_\n\n_3 - Rias Gremory_\n\n_4 - Zero Two_\n\n_5 - Chika MD_\n\n_6 - Miku MD_\n\n_7 - Marin Kitagawa_\n\n\nExample: *${prefix}setchar 0*\n`;
 
     let botLogos = [
       "https://wallpapercave.com/wp/wp5924545.jpg",
@@ -22,8 +22,8 @@ module.exports = {
     let randomimage = botLogos[Math.floor(Math.random() * botLogos.length)];
    
     let sections = [] 
-    let chars = [`Atlas MD`, `Power MD`, `Makima MD`, `Denji MD`, `Zero Two`, `Chika MD`, `Miku MD`, `Marin Kitagawa`]
-    let buttonDesc =[`Set bot character to Atlas MD`, `Set bot character to Power MD`, `Set bot character to Makima MD`, `Set bot character to Denji MD`, `Set bot character to Zero Two`, `Set bot character to Chika MD`, `Set bot character to Miku MD`, `Set bot character to Marin Kitagawa`]
+    let chars = [`Atlas MD`, `Power MD`, `Makima MD`, `Rias Gremory MD`, `Zero Two`, `Chika MD`, `Miku MD`, `Marin Kitagawa`]
+    let buttonDesc =[`Set bot character to Atlas MD`, `Set bot character to Power MD`, `Set bot character to Makima MD`, `Set bot character to Rias Gremory`, `Set bot character to Zero Two`, `Set bot character to Chika MD`, `Set bot character to Miku MD`, `Set bot character to Marin Kitagawa`]
     let buttonTexts = ['-setchar 0', '-setchar 1', '-setchar 2', '-setchar 3', '-setchar 4', '-setchar 5', '-setchar 6', '-setchar 7']
     
     for (let i = 0; i < chars.length; i++) {
